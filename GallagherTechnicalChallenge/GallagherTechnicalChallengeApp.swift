7f74fb2342c08552f9b59e69ff52d4023ae3e7a9
Nilusha Wimalasena <nilusha.wimalasena@trustpayments.com>
2022-07-16 20:19:21 +0530

    Initial Commit

---
+++ GallagherTechnicalChallenge/GallagherTechnicalChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GallagherTechnicalChallengeApp.swift
+//  GallagherTechnicalChallenge
+//
+//  Created by Nilusha Wimalasena on 2022-07-16.
+//
+
+import SwiftUI
+
+@main
+struct GallagherTechnicalChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
